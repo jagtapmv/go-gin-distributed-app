60f6f59be1843b5a909481d0d49867bd3412c8f4
Mahesh <jagtapmv991@gmail.com>
2022-08-06 15:16:48 +0530

    Changed output format to XML

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/:name", handleName)
+	router.GET("/", handleName)
 
 	router.Run()
 }
 
 func handleName(c *gin.Context) {
-	userName := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"Message": "Hello " + userName,
+	type Person struct {
+		XMLName   xml.Name `xml:"person"`
+		FirstName string   `xml:"firstname,attr"`
+		LastName  string   `xml:"lastname,attr"`
+	}
+	c.XML(200, Person{
+		FirstName: "Mahesh",
+		LastName:  "Jagtap",
 	})
 }
