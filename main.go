f98ae3c8dc59200d944bf419655d0c87484f56e8
Mahesh <jagtapmv991@gmail.com>
2022-08-06 16:16:35 +0530

    Real project started - Get and post Recipes

--- main.go
+++ main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
-	"encoding/xml"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal(file, &recipes)
+}
+
+func NewRecipeHandler(ctx *gin.Context) {
+	var recipe Recipe
+
+	if err := ctx.ShouldBindJSON(&recipe); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Error": err.Error(),
+		})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	ctx.JSON(http.StatusOK, recipe)
+}
+
+func ListRecipeHendler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, recipes)
+}
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", handleName)
+	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipeHendler)
 
 	router.Run()
-}
-
-func handleName(c *gin.Context) {
-	type Person struct {
-		XMLName   xml.Name `xml:"person"`
-		FirstName string   `xml:"firstname,attr"`
-		LastName  string   `xml:"lastname,attr"`
-	}
-	c.XML(200, Person{
-		FirstName: "Mahesh",
-		LastName:  "Jagtap",
-	})
 }
